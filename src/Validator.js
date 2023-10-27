d39cc810cc77ab88df6603978d74432b2ff949b4
Goga-Rid <wopa22@list.ru>
2023-10-27 12:49:13 +0300

    [Feature] tasks complete

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,18 @@
+import StringSchema from './StringSchema.js';
+import ArraySchema from './ArraySchema.js';
+import ObjectSchema from './ObjectSchema.js';
 
+class Validator {
+  string() {
+    return new StringSchema();
+  }
+
+  array() {
+    return new ArraySchema();
+  }
+
+  object() {
+    return new ObjectSchema();
+  }
+}
+export default Validator;
